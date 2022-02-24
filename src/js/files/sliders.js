1388472f18d090067082593617db8966bc4db241
Boris <boris021@mail.ru>
2022-02-24 12:23:25 +0500

    fix

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -28,6 +28,67 @@ import "../../scss/libs/swiper.scss";
 // Инициализация слайдеров
 function initSliders() {
 	// Перечень слайдеров
+	if (document.querySelector('.swiper-hit')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.swiper-hit', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 4,
+			spaceBetween: 120,
+			autoHeight: false,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.hit__body-arrow-prev',
+				nextEl: '.hit__body-arrow-next',
+			},
+
+			// Брейкпоинты
+
+			/*breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 1,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 1,
+					spaceBetween: 20,
+				},
+				1920: {
+					slidesPerView: 1.4,
+					spaceBetween: 20,
+				},
+			},*/
+
+			// События
+			on: {
+
+			}
+		});
+	}
 	// Проверяем, есть ли слайдер на стронице
 	if (document.querySelector('.swiper')) { // Указываем скласс нужного слайдера
 		// Создаем слайдер
@@ -91,6 +152,8 @@ function initSliders() {
 		});
 	}
 }
+
+
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
 	// Добавление классов слайдера
